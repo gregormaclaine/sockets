d8808deabf1c511432254e93a3d9bcd7d2353f90
Gregor Maclaine <thegregormaclaine@gmail.com>
2020-04-02 23:38:37 +0100

    Added ConnectionRefusedError handling

--- client.py
+++ client.py
@@ -8,13 +8,17 @@ else:
 
 PORT = 23
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.connect((HOST, PORT))
+  try:
+    s.connect((HOST, PORT))
+  except ConnectionRefusedError:
+    print(f'Error: Could not connect with host ({HOST}:{PORT})')
+    exit()
 
   while True:
     i = ''
     while i == '':
       i = input('>>>')
-      
+
     s.send(i.encode())
     if i == 'quit':
       print('Connection with host closed.')
